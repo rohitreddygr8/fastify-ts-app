1741e1c562d342b05152146916821b49956ceec1
rohitman47 <rohitreddy.gangwar@gmail.com>
2023-06-03 23:49:30 +0530

    feat: 🎸 add fastify plugins

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import path from 'node:path';
 
+import acceptsPlugin from '@fastify/accepts';
 import cookiePlugin from '@fastify/cookie';
 import eTagPlugin from '@fastify/etag';
 import formBodyPlugin from '@fastify/formbody';
@@ -7,11 +8,15 @@ import helmetPlugin from '@fastify/helmet';
 import multipartPlugin from '@fastify/multipart';
 import rateLimitPlugin from '@fastify/rate-limit';
 import staticPlugin from '@fastify/static';
+import viewPlugin from '@fastify/view';
+import webSocketsPlugin from '@fastify/websocket';
 import chalk from 'chalk';
 import { fastify, FastifyPluginAsync } from 'fastify';
+import handlebars from 'handlebars';
 
 import { ENV } from './constants/env-vars.js';
 import { authRouterPlugin } from './routes/v1/auth.route.js';
+import { viewRouterPlugin } from './routes/v1/view.route.js';
 
 export const app = fastify({
 	logger: ENV.LOGS_ENABLED && {
@@ -27,6 +32,7 @@ export const app = fastify({
 
 const v1Plugin: FastifyPluginAsync = async (app) => {
 	await app.register(authRouterPlugin, { prefix: '/auth' });
+	await app.register(viewRouterPlugin, { prefix: '/view' });
 };
 
 await app.register(rateLimitPlugin, {
@@ -39,11 +45,26 @@ await app.register(staticPlugin, {
 	prefix: '/public',
 });
 
-await app.register(helmetPlugin);
+await app.register(webSocketsPlugin, {
+	connectionOptions: { encoding: 'utf-8' },
+});
 
-await app.register(eTagPlugin);
+await app.register(viewPlugin, {
+	engine: { handlebars },
+	root: path.resolve('./src/views'),
+	viewExt: 'handlebars',
+	defaultContext: {
+		dev: ENV.IS_DEV,
+	},
+});
+
+await app.register(helmetPlugin, { contentSecurityPolicy: false });
 
 await app.register(cookiePlugin);
+
+await app.register(acceptsPlugin);
+
+await app.register(eTagPlugin);
 
 await app.register(formBodyPlugin);
 
@@ -54,22 +75,7 @@ await app.register(v1Plugin, { prefix: '/v1' });
 app.decorateRequest('user', null);
 
 app.get('/', (_, reply) => {
-	return reply.type('text/html; charset=utf-8;').send(
-		`<pre style="text-align:center;position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);">
-███████╗░█████╗░░██████╗████████╗██╗███████╗██╗░░░██╗	░░░░░░
-██╔════╝██╔══██╗██╔════╝╚══██╔══╝██║██╔════╝╚██╗░██╔╝	░░░░░░
-█████╗░░███████║╚█████╗░░░░██║░░░██║█████╗░░░╚████╔╝░	█████╗
-██╔══╝░░██╔══██║░╚═══██╗░░░██║░░░██║██╔══╝░░░░╚██╔╝░░	╚════╝
-██║░░░░░██║░░██║██████╔╝░░░██║░░░██║██║░░░░░░░░██║░░░	░░░░░░
-╚═╝░░░░░╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░╚═╝╚═╝░░░░░░░░╚═╝░░░	░░░░░░
-
-████████╗██╗░░░██╗██████╗░███████╗░██████╗░█████╗░██████╗░██╗██████╗░████████╗
-╚══██╔══╝╚██╗░██╔╝██╔══██╗██╔════╝██╔════╝██╔══██╗██╔══██╗██║██╔══██╗╚══██╔══╝
-░░░██║░░░░╚████╔╝░██████╔╝█████╗░░╚█████╗░██║░░╚═╝██████╔╝██║██████╔╝░░░██║░░░
-░░░██║░░░░░╚██╔╝░░██╔═══╝░██╔══╝░░░╚═══██╗██║░░██╗██╔══██╗██║██╔═══╝░░░░██║░░░
-░░░██║░░░░░░██║░░░██║░░░░░███████╗██████╔╝╚█████╔╝██║░░██║██║██║░░░░░░░░██║░░░
-░░░╚═╝░░░░░░╚═╝░░░╚═╝░░░░░╚══════╝╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░░░░░░░╚═╝░░░</pre>`,
-	);
+	return reply.view('home');
 });
 
 export const startServer = async () => {
